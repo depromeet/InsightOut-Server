6fc38e91ffc517cd2fcfc871162d78f2528580a1
Seongtae <stae1102@gmail.com>
2023-05-14 22:30:04 +0900

    feat: auth 임시 api 생성

--- src/apps/server/auth/auth.controller.ts
+++ src/apps/server/auth/auth.controller.ts
@@ -1,6 +1,24 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Post } from '@nestjs/common';
+import {
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { SigninRequestBodyDto } from './dtos/signin-request-body.dto';
 
 @ApiTags('Auth')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Post('signin')
+  @ApiOperation({
+    summary: '소셜 로그인',
+    description: '구글/애플/카카오 로그인',
+  })
+  @ApiUnauthorizedResponse({
+    status: 401,
+    description: '유효하지 않은 Token입니다.',
+  })
+  signIn(@Body() body: SigninRequestBodyDto) {
+    console.log(body);
+  }
+}
