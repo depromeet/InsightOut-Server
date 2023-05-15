3228dafbc4c2ba36b86c80d959b34fe460786775
Seongtae <stae1102@gmail.com>
2023-05-15 21:43:46 +0900

    feat: Redis cache 모듈 auth 도메인에 추가

--- src/apps/server/auth/auth.module.ts
+++ src/apps/server/auth/auth.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { SigninGuard } from '../guards/signin.guard';
+import { RedisCacheModule } from '../../../modules/cache/redis/redis.module';
 
 @Module({
+  imports: [RedisCacheModule],
   controllers: [AuthController],
   providers: [AuthService, SigninGuard],
 })
