c2ebde774c994d6729625199eb897051a9dddfa7
Seongtae <stae1102@gmail.com>
2023-05-16 20:59:29 +0900

    feat: jwt module 임포트 등록

--- src/apps/server/auth/auth.module.ts
+++ src/apps/server/auth/auth.module.ts
@@ -3,9 +3,20 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { SigninGuard } from '../guards/signin.guard';
 import { RedisCacheModule } from '../../../modules/cache/redis/redis.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [RedisCacheModule],
+  imports: [
+    RedisCacheModule,
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => {
+        return {
+          secret: configService.get<string>('JWT_SECRET'),
+        };
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, SigninGuard],
 })
