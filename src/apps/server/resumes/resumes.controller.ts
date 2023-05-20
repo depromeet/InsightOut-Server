ff06479b446db1e038cfcd6de21b57ed866e35cf
Seongtae <stae1102@gmail.com>
2023-05-20 23:42:56 +0900

    feat: Resume controller 및 Read 메서드 추가

---
+++ src/apps/server/resumes/resumes.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ResumesService } from './resumes.service';
+import { User } from '../decorators/request/user.decorator';
+import { UserJwtToken } from '../auth/types/jwt-tokwn.type';
+import { ResponseEntity } from '../../../libs/utils/respone.entity';
+
+@ApiTags('resumes')
+@Controller('resumes')
+export class ResumesController {
+  constructor(private readonly resumesService: ResumesService) {}
+
+  @Get()
+  @ApiOperation({
+    summary: '자기소개서를 조회',
+    description:
+      '자기소개서를 처음 조회했을 때, 자기소개서 폴더링 목록과 각 폴더링 별 문항을 모두 출력합니다.',
+  })
+  async getAllResumes(@User() user: UserJwtToken) {
+    const resumes = await this.resumesService.getAllResumes(user.userId);
+
+    return ResponseEntity.OK_WITH_DATA(resumes);
+  }
+}
