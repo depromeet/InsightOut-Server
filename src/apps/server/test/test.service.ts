c8ee9e373d40002032b3f9cd11867b40a97d9439
Seongtae <stae1102@gmail.com>
2023-05-21 18:03:39 +0900

    refactor: 테스트 토큰에 refresh 토큰 추가

--- src/apps/server/test/test.service.ts
+++ src/apps/server/test/test.service.ts
@@ -2,21 +2,25 @@ import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { ACCESS_TOKEN_EXPIRES_IN } from '../consts/jwt.const';
 import { ConfigService } from '@nestjs/config';
+import { RedisCacheService } from '../../../modules/cache/redis/redis.service';
 
 @Injectable()
 export class TestService {
   constructor(
+    private readonly redisService: RedisCacheService,
     private readonly jwtService: JwtService,
     private readonly configService: ConfigService,
   ) {}
 
-  issueTestToken(userId: number) {
-    return this.jwtService.sign(
+  async issueTestToken(userId: number) {
+    const token = this.jwtService.sign(
       { userId },
       {
         expiresIn: ACCESS_TOKEN_EXPIRES_IN,
         secret: this.configService.get('JWT_SECRET'),
       },
     );
+    await this.redisService.set(String(userId), token);
+    return token;
   }
 }
