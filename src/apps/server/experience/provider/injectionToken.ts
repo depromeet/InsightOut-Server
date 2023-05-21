0f0651059c7b5901e38441d3a22064e250f9f7ce
PracticeEveryday <developerforus@gmail.com>
2023-05-22 08:35:45 +0900

    feat: injectionToken 생성

---
+++ src/apps/server/experience/provider/injectionToken.ts
@@ -0,0 +1,5 @@
+export enum ExperienceToken {
+  EXPERIENCE_REPOSITORY = 'ExperienceRepository',
+  EXPERIENCE_INFO_REPOSITORY = 'ExperienceInfoRepository',
+  EXPERIENCE_TRANSACTION_REPOSITORY = 'ExperienceTransactionRepository',
+}
