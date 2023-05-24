2ffeb4ba7d91fa7a6be3625815e119567907ab0b
Seongtae <stae1102@gmail.com>
2023-05-25 00:27:29 +0900

    feat: 자기소개서 문항 추가 api handler 추가

--- src/apps/server/resumes/services/question.service.ts
+++ src/apps/server/resumes/services/question.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { QuestionRepository } from '../../../../modules/database/repositories/question.repository';
+import { ResumeRepository } from '../../../../modules/database/repositories/resume.repository';
+import { PostQuestionResponseDto } from '../dtos/post-question.dto';
 
 @Injectable()
-export class QuestionService {}
+export class QuestionsService {
+  constructor(
+    private readonly resumeRepository: ResumeRepository,
+    private readonly questionRepository: QuestionRepository,
+  ) {}
+
+  async createOneQuestion(
+    userId: number,
+    resumeId: number,
+  ): Promise<PostQuestionResponseDto> {
+    const resume = await this.resumeRepository.findFirst({
+      where: { id: resumeId, User: { id: userId } },
+      include: { Question: true },
+    });
+
+    if (!resume) {
+      throw new NotFoundException('Resume not found');
+    }
+
+    const questionLength = resume['Question'].length;
+
+    const question = await this.questionRepository.create({
+      data: { resumeId, title: `${questionLength + 1}번 문항` },
+    });
+
+    const questionReponseDto = new PostQuestionResponseDto(question);
+    return questionReponseDto;
+  }
+}
