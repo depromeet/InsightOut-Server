981647af13f0cf0b4c972bb5fc762b5fcfef21f3
Seongtae <stae1102@gmail.com>
2023-05-24 20:47:40 +0900

    feat: 자기소개서 추가 api handler 추가

--- src/apps/server/resumes/resumes.service.ts
+++ src/apps/server/resumes/resumes.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { Resume } from '@prisma/client';
 import { ResumesRepository } from '../../../modules/database/repositories/resume.repository';
+import {
+  PostResumeRequestBodyDto,
+  PostResumeResponseDto,
+} from './dtos/post-resume.dto';
 
 @Injectable()
 export class ResumesService {
@@ -12,5 +16,19 @@ export class ResumesService {
     });
 
     return resumes;
+  }
+
+  async createResumeFolder(
+    body: PostResumeRequestBodyDto,
+    userId: number,
+  ): Promise<PostResumeResponseDto> {
+    const { title } = body;
+    const resume = await this.resumesRepository.create({
+      data: { title, userId },
+    });
+
+    // Entity -> DTO
+    const resumeResponseDto = new PostResumeResponseDto(resume);
+    return resumeResponseDto;
   }
 }
