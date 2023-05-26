8ce9aa5f663f6ef6a919a39d56cf3219fd56ff60
Seongtae <stae1102@gmail.com>
2023-05-26 22:07:49 +0900

    feat: 자기소개서 조회 API 옵션 적용

--- src/apps/server/resumes/services/resumes.service.ts
+++ src/apps/server/resumes/services/resumes.service.ts
@@ -5,15 +5,23 @@ import {
   PostResumeRequestBodyDto,
   PostResumeResponseDto,
 } from '../dtos/post-resume.dto';
+import { GetResumeRequestQueryDto } from '@apps/server/resumes/dtos/get-resume-query.dto';
 
 @Injectable()
 export class ResumesService {
   constructor(private readonly resumesRepository: ResumeRepository) {}
 
-  async getAllResumes(userId: number): Promise<Resume[]> {
+  async getAllResumes(
+    userId: number,
+    query?: GetResumeRequestQueryDto,
+  ): Promise<Resume[]> {
     const resumes = await this.resumesRepository.findMany({
       where: { userId },
+      include: { Question: query.question },
     });
+    if (query && query.filter) {
+      return resumes.filter((resume) => resume.title === query.filter);
+    }
 
     return resumes;
   }
