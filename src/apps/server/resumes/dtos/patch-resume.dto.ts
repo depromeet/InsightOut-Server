089876308de85c5efc26b4d2b0f71ec9b246ab50
Seongtae <stae1102@gmail.com>
2023-05-27 22:00:08 +0900

    [Feature] 자기소개서 폴더 수정 API (#55)
    * feat: 자기소개서 수정 DTO
    
    * feat: 자기소개서 수정 API endpoint 추가
    
    * feat: 자기소개서 수정 API handler 추가
    
    * feat: 자기소개서 수정 API 타입 지정

---
+++ src/apps/server/resumes/dtos/patch-resume.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class PatchResumeRequestDto {
+  @ApiProperty({
+    description: '자기소개서 폴더 제목',
+    example: '디프만 13기',
+  })
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+}
---
+++ src/apps/server/resumes/resumes.service.ts
@@ -0,0 +1,48 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Resume } from '@prisma/client';
+import { PostResumeRequestBodyDto, PostResumeResponseDto } from './dtos/post-resume.dto';
+import { PatchResumeRequestDto } from './dtos/patch-resume.dto';
+import { ResumeRepository } from '@modules/database/repositories/resume.repository';
+
+@Injectable()
+export class ResumesService {
+  constructor(private readonly resumesRepository: ResumeRepository) {}
+
+  async getAllResumes(userId: number): Promise<Resume[]> {
+    const resumes = await this.resumesRepository.findMany({
+      where: { userId },
+    });
+
+    return resumes;
+  }
+
+  async createResumeFolder(body: PostResumeRequestBodyDto, userId: number): Promise<PostResumeResponseDto> {
+    const { title } = body;
+    const resume = await this.resumesRepository.create({
+      data: { title, userId },
+    });
+
+    // Entity -> DTO
+    const resumeResponseDto = new PostResumeResponseDto(resume);
+    return resumeResponseDto;
+  }
+
+  async updateResumeFolder({ body, resumeId, userId }: { body: PatchResumeRequestDto; resumeId: number; userId: number }): Promise<void> {
+    const { title } = body;
+
+    const resume = await this.resumesRepository.findFirst({
+      where: { id: resumeId, userId },
+    });
+
+    if (!resume) {
+      throw new NotFoundException('Resume not found');
+    }
+
+    if (title) {
+      await this.resumesRepository.update({
+        data: { title },
+        where: { id: resumeId },
+      });
+    }
+  }
+}
