0e25d9e99480b1c57bbd6cd15100a02da5a4e220
PracticeEveryday <developerforus@gmail.com>
2023-05-28 14:04:21 +0900

    refactor: transaction 서비스 단에서 처리

--- src/apps/server/experiences/experience.service.ts
+++ src/apps/server/experiences/experience.service.ts
@@ -1,39 +1,60 @@
 import { Inject, Injectable, NotFoundException, UnprocessableEntityException } from '@nestjs/common';
 import { CreateExperienceInfoReqDto } from './dto/req/createExperienceInfo.dto';
 import { UserJwtToken } from '../auth/types/jwt-tokwn.type';
-import { ExperienceReposirotyInterface, ExperienceTransactionInterface } from './interface/experience-repository.interface';
+import { ExperienceReposirotyInterface } from './interface/experience-repository.interface';
 import { ExperienceToken } from './provider/injectionToken';
 import { CreateExperienceResDto } from './dto/res/createExperienceInfo.res.dto';
 import { returnValueToDto } from '../common/decorators/returnValueToDto';
 import { getExperienceAttribute } from '../common/consts/experience-attribute.const';
 import { GetExperienceResDto } from './dto/res/getExperience.res.dto';
+import { ExperienceStatus, Prisma } from '@prisma/client';
+import { PrismaService } from '@modules/database/prisma.service';
 
 @Injectable()
 export class ExperienceService {
   constructor(
-    @Inject(ExperienceToken.EXPERIENCE_TRANSACTION_REPOSITORY)
-    private readonly experienceTransactionRepository: ExperienceTransactionInterface,
-
     @Inject(ExperienceToken.EXPERIENCE_REPOSITORY)
     private readonly experienceRepository: ExperienceReposirotyInterface,
+    private readonly prisma: PrismaService,
   ) {}
 
   @returnValueToDto(CreateExperienceResDto)
   public async createExperienceInfo(body: CreateExperienceInfoReqDto, user: UserJwtToken): Promise<CreateExperienceResDto> {
-    const [experience, experienceInfo] = await this.experienceTransactionRepository.createExperienceInfo(body, user);
-    if (!experience || !experienceInfo) throw new UnprocessableEntityException('경험 카드 생성하는 데 실패했습니다. 타입을 확인해주세요');
+    try {
+      return await this.prisma.$transaction(async (tx) => {
+        const experience = await tx.experience.create({
+          data: {
+            title: body.title,
+            startDate: body.startDate,
+            endDate: body.endDate,
+            experienceStatus: ExperienceStatus.inprogress,
+            userId: user.userId,
+          },
+        });
 
-    return {
-      experienceId: experience.id,
-      title: experience.title,
-      startDate: experience.startDate,
-      endDate: experience.endDate,
-      experienceInfo: {
-        experienceInfoId: experienceInfo.experienceInfoId,
-        experienceRole: experienceInfo.experienceRole,
-        motivation: experienceInfo.motivation,
-      },
-    };
+        const experienceInfo = await tx.experienceInfo.create({
+          data: {
+            experienceRole: body.experienceRole,
+            motivation: body.motivation,
+            experienceId: experience.id,
+          },
+        });
+        return {
+          experienceId: experience.id,
+          title: experience.title,
+          startDate: experience.startDate,
+          endDate: experience.endDate,
+          experienceInfo: {
+            experienceInfoId: experienceInfo.experienceInfoId,
+            experienceRole: experienceInfo.experienceRole,
+            motivation: experienceInfo.motivation,
+          },
+        };
+      });
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientValidationError)
+        throw new UnprocessableEntityException('경험 카드 생성하는 데 실패했습니다. 타입을 확인해주세요');
+    }
   }
 
   @returnValueToDto(GetExperienceResDto)
