be81878449eb5928efcdc1c1ed219a3906f3a11d
Seongtae <stae1102@gmail.com>
2023-05-28 11:47:21 +0900

    feat: firebase 사용하지 않는 코드 삭제

--- src/modules/firebase/firebase.service.ts
+++ src/modules/firebase/firebase.service.ts
@@ -1,16 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { getAuth } from 'firebase-admin/auth';
 
 @Injectable()
 export class FirebaseService {
-  constructor(private readonly configService: ConfigService) {}
-  private readonly firebaseConfig = {
-    apiKey: this.configService.get('FIREBASE_API_KEY'),
-    projectId: this.configService.get('FIREBASE_PROJECT_ID'),
-    clientEmail: this.configService.get('FIREBASE_CLIENT_EMAIL'),
-  };
-
   async verifyIdToken(idToken: string) {
     const decodedToken = await getAuth().verifyIdToken(idToken);
 
