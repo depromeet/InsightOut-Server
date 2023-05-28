edca6120ebeba7e6e5179d76dba2acf58d2c4c34
PracticeEveryday <developerforus@gmail.com>
2023-05-28 22:05:45 +0900

    refactor: getter response로 변경

--- src/apps/server/experiences/dto/res/getExperience.res.dto.ts
+++ src/apps/server/experiences/dto/res/getExperience.res.dto.ts
@@ -1,63 +1,100 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { ExperienceStatus } from '@prisma/client';
-import { Expose } from 'class-transformer';
+import { Exclude, Expose } from 'class-transformer';
 import { IsEnum, IsOptional, Matches } from 'class-validator';
 import { dateValidation } from '🔥apps/server/common/consts/date-validation.const';
 import { IsOptionalNumber } from '🔥apps/server/common/decorators/validation/isOptionalNumber.decorator';
 import { IsOptionalString } from '🔥apps/server/common/decorators/validation/isOptionalString.decorator';
 
 export class GetExperienceInfoResDto {
-  @ApiProperty({ example: 1 })
-  @IsOptionalNumber()
-  experienceInfoId: number;
+  @Exclude() _experienceInfoId: number;
+  @Exclude() _experienceId: number;
+  @Exclude() _motivation: string;
+  @Exclude() _experienceRole: string;
+  @Exclude() _utilization: string;
+  @Exclude() _analysis: string;
 
   @ApiProperty({ example: 1 })
   @IsOptionalNumber()
-  experienceId: number;
+  get experienceInfoId(): number {
+    return this._experienceInfoId;
+  }
+
+  @ApiProperty({ example: 1 })
+  @IsOptionalNumber()
+  get experienceId(): number {
+    return this._experienceId;
+  }
 
   @ApiPropertyOptional({
     example: '개발자와 협업 역량을 기르기 위해 하게 됨',
   })
   @IsOptionalString(0, 100)
-  motivation: string;
+  get motivation(): string {
+    return this._motivation;
+  }
 
   @ApiPropertyOptional({
     example: 'UI/UX 디자이너',
   })
   @IsOptionalString(0, 100)
-  experienceRole: string;
+  get experienceRole(): string {
+    return this._experienceRole;
+  }
 
   @ApiPropertyOptional({
     example: '역량 활용',
   })
   @IsOptionalString(0, 100)
-  utilization: string;
+  get utilization(): string {
+    return this._utilization;
+  }
 
   @ApiPropertyOptional({
     example: 'AI 분석',
   })
   @IsOptionalString(0, 100)
-  analysis: string;
+  get analysis(): string {
+    return this._analysis;
+  }
 }
 
 export class GetExperienceResDto {
+  @Exclude() _id: number;
+  @Exclude() _title: string;
+  @Exclude() _startDate: string;
+  @Exclude() _endDate: string;
+  @Exclude() _experienceStatus: ExperienceStatus;
+  @Exclude() _situation: string;
+  @Exclude() _task: string;
+  @Exclude() _action: string;
+  @Exclude() _result: string;
+
   @ApiProperty({ example: 1 })
   @IsOptionalNumber()
-  id: number;
+  get id(): number {
+    return this._id;
+  }
 
   @ApiPropertyOptional({ example: '00직무 디자인 인턴' })
   @IsOptionalString(0, 100)
-  title: string;
+  get title(): string {
+    return this._title;
+  }
 
   @ApiPropertyOptional({ example: '2022-01' })
   @IsOptionalString(0, 7)
   @Matches(dateValidation.YYYY_MM)
-  startDate: string;
+  get startDate(): string {
+    return this._startDate;
+  }
 
   @ApiPropertyOptional({ example: '2022-07' })
   @IsOptionalString(0, 7)
   @Matches(dateValidation.YYYY_MM)
-  endDate: string;
+  get endDate(): string {
+    return this._endDate;
+  }
 
   @ApiPropertyOptional({
     example: 'INPROGRESS or DONE',
@@ -66,23 +103,33 @@ export class GetExperienceResDto {
   @IsEnum(ExperienceStatus)
   @IsOptional()
   @Expose()
-  experienceStatus: ExperienceStatus;
+  get experienceStatus(): ExperienceStatus {
+    return this._experienceStatus;
+  }
 
   @ApiPropertyOptional({ example: '개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감' })
   @IsOptionalString(0, 100)
-  situation: string;
+  get situation(): string {
+    return this._situation;
+  }
 
   @ApiProperty({ example: '개발 시간이 짧아서 빠른 기간 내에 런칭을 완료해야 했음' })
   @IsOptionalString(0, 100)
-  task: string;
+  get task(): string {
+    return this._task;
+  }
 
   @ApiPropertyOptional({ example: '디자인 시스템 제작, 런칭일 정해서 린하게 개발하는 방법 제의' })
   @IsOptionalString(0, 100)
-  action: string;
+  get action(): string {
+    return this._action;
+  }
 
   @ApiPropertyOptional({ example: '4개월만에 출시를 성공하게 됨' })
   @IsOptionalString(0, 100)
-  result: string;
+  get result(): string {
+    return this._result;
+  }
 
   @Expose()
   @ApiProperty({ type: GetExperienceInfoResDto })
