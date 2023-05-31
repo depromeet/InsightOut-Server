fa1b144404b8c69afba5bd350a75d7a9a630228c
Seongtae <stae1102@gmail.com>
2023-06-01 01:27:42 +0900

    feat: Capability 레포지터리 추가

---
+++ src/libs/modules/database/repositories/capability.repository.ts
@@ -0,0 +1,15 @@
+import { Prisma } from '@prisma/client';
+import { PrismaService } from '📚libs/modules/database/prisma.service';
+import { AbstractRepository, DelegateArgs, DelegateReturnTypes } from '📚libs/modules/database/repositories/abstract.repository';
+
+type CapabilityDelegate = Prisma.CapabilityDelegate<Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined>;
+
+export class CapabilityRepository extends AbstractRepository<
+  CapabilityDelegate,
+  DelegateArgs<CapabilityDelegate>,
+  DelegateReturnTypes<CapabilityDelegate>
+> {
+  constructor(private readonly prisma: PrismaService) {
+    super(prisma.capability);
+  }
+}
