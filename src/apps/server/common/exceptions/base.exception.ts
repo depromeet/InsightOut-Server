755b2af4e457be6a3b5c619dfeae650a108ec130
Seongtae <stae1102@gmail.com>
2023-06-01 21:32:06 +0900

    feat: response entity와 log 출력 수정

--- src/apps/server/common/exceptions/base.exception.ts
+++ src/apps/server/common/exceptions/base.exception.ts
@@ -1,34 +1,51 @@
 import { HttpException } from '@nestjs/common';
 import { ApiProperty } from '@nestjs/swagger';
-import { Exclude } from 'class-transformer';
+import { Exclude, Expose } from 'class-transformer';
 
 export class BaseException extends HttpException {
-  @ApiProperty({ description: '응답코드' })
-  statusCode: number;
+  @Exclude() private readonly _statusCode: number;
+  @Exclude() private readonly _success = false;
+  @Exclude() raw: Error;
+  @Exclude() private readonly _title: string;
 
-  @ApiProperty({ description: '에러 제목' })
-  title: string;
-
-  @ApiProperty({ description: '에러 메시지' })
-  message: string;
-
-  @Exclude()
-  raw: Error;
-
-  constructor(
-    properties: Pick<BaseException, 'statusCode' | 'title' | 'message' | 'raw'>,
-  ) {
+  constructor(properties: Pick<BaseException, 'statusCode' | 'title' | 'message' | 'raw'>) {
     super(properties.message, properties.statusCode);
-    this.title = properties.title;
-    this.statusCode = properties.statusCode;
+    this._title = properties.title;
+    this._statusCode = properties.statusCode;
+    this._success = false;
     this.raw = properties.raw;
   }
 
   getResponse() {
     return {
       statusCode: this.statusCode,
-      title: this.title,
-      message: this.message,
+      success: false,
+      data: {
+        title: this.title,
+        message: this.message,
+      },
     };
   }
+
+  @ApiProperty({ description: '응답코드' })
+  @Expose()
+  get statusCode(): number {
+    return this._statusCode;
+  }
+
+  @ApiProperty({ description: '에러 제목' })
+  @Expose()
+  get title(): string {
+    return this._title;
+  }
+
+  @ApiProperty({ description: 'API 요청 성공 유무' })
+  @Expose()
+  get success(): boolean {
+    return this._success;
+  }
+
+  @ApiProperty({ description: '에러 메시지' })
+  @Expose()
+  message: string;
 }
