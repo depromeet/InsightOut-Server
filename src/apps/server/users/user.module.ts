c841f4c1db65f9281288c5365989db334970a67d
Seongtae <stae1102@gmail.com>
2023-06-03 03:16:43 +0900

    feat: 피드백 생성 API handler 추가

--- src/apps/server/users/user.module.ts
+++ src/apps/server/users/user.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { FeedbackRepository } from '📚libs/modules/database/repositories/feedback.repository';
 import { UserRepository } from '📚libs/modules/database/repositories/user.repository';
 import { UserController } from '🔥apps/server/users/user.controller';
 import { UserService } from '🔥apps/server/users/user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, UserRepository],
+  providers: [UserService, UserRepository, FeedbackRepository],
 })
 export class UserModule {}
--- src/apps/server/users/user.service.ts
+++ src/apps/server/users/user.service.ts
@@ -1,7 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { FeedbackRepository } from '📚libs/modules/database/repositories/feedback.repository';
 import { UserRepository } from '📚libs/modules/database/repositories/user.repository';
+import { PostSendFeedbackRequestBodyDto } from '🔥apps/server/users/dtos/post-feedback.dto';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(private readonly userRepository: UserRepository, private readonly feedbackRepository: FeedbackRepository) {}
+
+  /**
+   * 유저가 작성한 피드백을 Feedback 테이블에 저장합니다.
+   *
+   * @param body 유저가 작성한 피드백이 담긴 객체입니다.
+   */
+  async sendFeedback(body: PostSendFeedbackRequestBodyDto): Promise<void> {
+    const { contents } = body;
+    await this.feedbackRepository.create({
+      data: { contents },
+    });
+  }
 }
