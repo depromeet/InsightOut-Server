d63c2f40de939a2f03f93fbabb7f991d7b313cba
Seongtae <stae1102@gmail.com>
2023-06-05 02:01:17 +0900

    feat: 경험 카드와 자기소개서 개수 조회 API handler 추가

--- src/apps/server/collections/collection.service.ts
+++ src/apps/server/collections/collection.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { ExperienceRepository } from '📚libs/modules/database/repositories/experience.repository';
 import { ResumeRepository } from '📚libs/modules/database/repositories/resume.repository';
+import { GetCountOfExperienceAndResumeResponseDto } from '🔥apps/server/collections/dtos/get-count-of-experience-and-resume.dto';
 
 @Injectable()
 export class CollectionsService {
   constructor(private readonly experienceRepository: ExperienceRepository, private readonly resumeRepository: ResumeRepository) {}
+
+  async getCountOfExperienceAndResume(userId: number): Promise<GetCountOfExperienceAndResumeResponseDto> {
+    const countOfExperience = await this.experienceRepository.countExperience(userId);
+
+    const countOfResume = await this.resumeRepository.count({
+      where: { userId },
+    });
+
+    const getCountOfExperienceAndResumeResponseDto = new GetCountOfExperienceAndResumeResponseDto(countOfExperience, countOfResume);
+
+    return getCountOfExperienceAndResumeResponseDto;
+  }
 }
