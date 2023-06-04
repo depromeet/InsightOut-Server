1eca51d986672f7eb9292cb7d767b903b9128ffb
Seongtae <stae1102@gmail.com>
2023-06-04 14:07:57 +0900

    feat: 온보딩 조회 API endpoint 추가

--- src/apps/server/onboarding/onboarding.controller.ts
+++ src/apps/server/onboarding/onboarding.controller.ts
@@ -1,7 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpStatus } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { Method } from '📚libs/enums/method.enum';
+import { ResponseEntity } from '📚libs/utils/respone.entity';
+import { UserJwtToken } from '🔥apps/server/auth/types/jwt-tokwn.type';
+import { User } from '🔥apps/server/common/decorators/request/user.decorator';
+import { Route } from '🔥apps/server/common/decorators/router/route.decorator';
+import {
+  GetOnboardingSummaryMd,
+  GetOnboardingDescriptionMd,
+  GetOnBoardingResponseDescription,
+} from '🔥apps/server/onboarding/docs/get-onboarding.doc';
 import { OnboardingsService } from '🔥apps/server/onboarding/onboarding.service';
 
+@ApiTags('🏂 온보딩 API')
 @Controller('onboardings')
 export class OnboardingsController {
   constructor(private readonly onboardingsService: OnboardingsService) {}
+
+  @Route({
+    request: {
+      path: '',
+      method: Method.GET,
+    },
+    response: {
+      code: HttpStatus.OK,
+      description: GetOnBoardingResponseDescription,
+    },
+    summary: GetOnboardingSummaryMd,
+    description: GetOnboardingDescriptionMd,
+  })
+  async getUserOnboardingInformation(@User() user: UserJwtToken) {
+    const onboarding = await this.onboardingsService.getUserOnboardingInformation(user.userId);
+
+    return ResponseEntity.OK_WITH_DATA(onboarding);
+  }
 }
