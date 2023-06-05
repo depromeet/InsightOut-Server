4e93f3943f7906b6d585686807f12b141a20d5eb
Seongtae <stae1102@gmail.com>
2023-06-06 01:26:22 +0900

    feat: 경험분해 조회 API 수정

--- src/apps/server/experiences/services/experience.service.ts
+++ src/apps/server/experiences/services/experience.service.ts
@@ -1,20 +1,20 @@
 import { Inject, Injectable, NotFoundException, UnprocessableEntityException } from '@nestjs/common';
 import { UpsertExperienceReqDto } from '../dto/req/upsertExperience.dto';
 import { UserJwtToken } from '../../auth/types/jwt-tokwn.type';
-import { ExperienceRepositoryInterface } from '../interface/experience-repository.interface';
 import { UpsertExperienceResDto } from '../dto/res/upsertExperienceInfo.res.dto';
 import { getExperienceAttribute } from '../../common/consts/experience-attribute.const';
-import { GetExperienceResDto } from '../dto/res/getExperience.res.dto';
+import { GetExperienceByCapabilityResponseDto, GetExperienceResDto } from '../dto/res/getExperience.res.dto';
 import { Experience, ExperienceInfo, ExperienceStatus, Prisma } from '@prisma/client';
 import { PrismaService } from '📚libs/modules/database/prisma.service';
 import { ExperienceRepository } from '📚libs/modules/database/repositories/experience.repository';
+import { CapabilityRepository } from '📚libs/modules/database/repositories/capability.repository';
 
 @Injectable()
 export class ExperienceService {
   constructor(
-    @Inject(ExperienceRepository)
-    private readonly experienceRepository: ExperienceRepositoryInterface,
+    private readonly experienceRepository: ExperienceRepository,
     private readonly prisma: PrismaService,
+    private readonly capabilityRepository: CapabilityRepository,
   ) {}
 
   public async upsertExperience(body: UpsertExperienceReqDto, user: UserJwtToken): Promise<UpsertExperienceResDto> {
@@ -46,12 +46,35 @@ export class ExperienceService {
     }
   }
 
+  public async getExperienceByCapability(capabilityId: number): Promise<GetExperienceByCapabilityResponseDto[]> {
+    const experience = await this.experienceRepository.getExperienceByCapability(capabilityId);
+    if (!experience.length) {
+      throw new NotFoundException('Experience not found');
+    }
+
+    const getExperienceByCapabilityResponseDto = experience.map((experience) => new GetExperienceByCapabilityResponseDto(experience));
+
+    return getExperienceByCapabilityResponseDto;
+  }
+
   public async getExperienceByUserId(userId: number): Promise<GetExperienceResDto | string> {
     try {
       const experience = await this.experienceRepository.selectOneByUserId(userId, getExperienceAttribute);
       if (!experience) return 'INPROGRESS 상태의 경험카드가 없습니다';
 
       return new GetExperienceResDto(experience);
+    } catch (error) {
+      console.log('error', error);
+      if (error instanceof Prisma.PrismaClientKnownRequestError) throw new NotFoundException('INPROGRESS 상태의 경험카드가 없습니다.');
+    }
+  }
+
+  public async getExperiencesByUserId(userId: number): Promise<GetExperienceResDto[] | string> {
+    try {
+      const experience = await this.experienceRepository.findManyByUserId(userId, getExperienceAttribute);
+      if (!experience.length) return 'INPROGRESS 상태의 경험카드가 없습니다';
+
+      return experience.map((experience) => new GetExperienceResDto(experience));
     } catch (error) {
       console.log('error', error);
       if (error instanceof Prisma.PrismaClientKnownRequestError) throw new NotFoundException('INPROGRESS 상태의 경험카드가 없습니다.');
