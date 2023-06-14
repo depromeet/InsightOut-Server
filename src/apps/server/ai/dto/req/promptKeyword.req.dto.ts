e278f0145b48abea73ff659b77c8b7297a690a8d
PracticeEveryday <developerforus@gmail.com>
2023-06-14 22:13:20 +0900

    feat: body property required로 변경

--- src/apps/server/ai/dto/req/promptKeyword.req.dto.ts
+++ src/apps/server/ai/dto/req/promptKeyword.req.dto.ts
@@ -4,17 +4,17 @@ import { IsNotEmptyString } from '🔥apps/server/common/decorators/validation/i
 export class PromptKeywordBodyReqDto {
   @ApiPropertyOptional({ example: '개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감' })
   @IsNotEmptyString(0, 100)
-  situation?: string;
+  situation: string;
 
   @ApiPropertyOptional({ example: '개발 기간이 짧아서 빠른 기간 내 런칭을 완료해야 했음.' })
   @IsNotEmptyString(0, 100)
-  task?: string;
+  task: string;
 
   @ApiPropertyOptional({ example: '디자인 시스템 제작, 런칭일 정해서 린하게 개발하는 방법 제의' })
   @IsNotEmptyString(0, 100)
-  action?: string;
+  action: string;
 
   @ApiPropertyOptional({ example: '4개월 만에 출시에 성공하게 됨.' })
   @IsNotEmptyString(0, 100)
-  result?: string;
+  result: string;
 }
