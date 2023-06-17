fe8639661bdf6241707418d7c6d04dd4c5598a7b
Seongtae <stae1102@gmail.com>
2023-06-17 20:38:40 +0900

    feat: token으로 인한 예외 타입 분리

--- src/apps/server/common/guards/jwt-auth.guard.ts
+++ src/apps/server/common/guards/jwt-auth.guard.ts
@@ -1,11 +1,8 @@
-import {
-  ExecutionContext,
-  Injectable,
-  NotFoundException,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { ExecutionContext, Injectable, NotFoundException } from '@nestjs/common';
 import { TokenExpiredError } from 'jsonwebtoken';
 import { AuthGuard } from '@nestjs/passport';
+import { TokenExpiredException } from '🔥apps/server/common/exceptions/token-expired.exception';
+import { TokenType } from '📚libs/enums/token.enum';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
@@ -13,15 +10,9 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     super();
   }
 
-  handleRequest<TUser = any>(
-    err: any,
-    user: any,
-    info: any,
-    _context: ExecutionContext,
-    _status?: any,
-  ): TUser {
+  handleRequest<TUser = any>(err: any, user: any, info: any, _context: ExecutionContext, _status?: any): TUser {
     if (info instanceof TokenExpiredError) {
-      throw new UnauthorizedException();
+      throw new TokenExpiredException(TokenType.AccessToken);
     }
 
     if (!user) {
--- src/apps/server/common/guards/jwt-refresh.guard.ts
+++ src/apps/server/common/guards/jwt-refresh.guard.ts
@@ -1,25 +1,17 @@
-import {
-  ExecutionContext,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { TokenExpiredError } from 'jsonwebtoken';
+import { TokenType } from '📚libs/enums/token.enum';
+import { TokenExpiredException } from '🔥apps/server/common/exceptions/token-expired.exception';
 
 @Injectable()
 export class JwtRefreshGuard extends AuthGuard('refresh') {
   constructor() {
     super();
   }
-  handleRequest(
-    _err: any,
-    user: any,
-    info: any,
-    _context: ExecutionContext,
-    _status?: any,
-  ) {
+  handleRequest(_err: any, user: any, info: any, _context: ExecutionContext, _status?: any) {
     if (info instanceof TokenExpiredError) {
-      throw new UnauthorizedException('만료된 refresh 토큰입니다.');
+      throw new TokenExpiredException(TokenType.RefreshToken);
     }
     if (!user) {
       throw new UnauthorizedException('적절하지 않은 요청입니다.');
