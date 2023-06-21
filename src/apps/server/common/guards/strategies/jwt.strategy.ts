a5a151a5eeea3f15f1056f8eee96b2fb96628a1d
Seongtae <stae1102@gmail.com>
2023-06-22 07:00:44 +0900

    feat: 액세스 토큰 추적을 쿠키로 변경

--- src/apps/server/common/guards/strategies/jwt.strategy.ts
+++ src/apps/server/common/guards/strategies/jwt.strategy.ts
@@ -1,16 +1,24 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Request } from 'express';
 import { UserRepository } from '📚libs/modules/database/repositories/user.repository';
 import { EnvEnum } from '📚libs/modules/env/env.enum';
 import { EnvService } from '📚libs/modules/env/env.service';
+import { AuthService } from '🔥apps/server/auth/auth.service';
 import { UserJwtToken } from '🔥apps/server/auth/types/jwt-tokwn.type';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(readonly envService: EnvService, private readonly userRepository: UserRepository) {
+  constructor(readonly authService: AuthService, readonly envService: EnvService, private readonly userRepository: UserRepository) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request): string => {
+          const accessToken = authService.extractAccessToken(request);
+          console.log(accessToken);
+          return accessToken;
+        },
+      ]),
       ignoreExipration: false,
       secretOrKey: envService.get<string>(EnvEnum.JWT_ACCESS_TOKEN_SECRET),
     });
