9cbed48599f086eab36fe4cdf381069cd81caa66
PracticeEveryday <developerforus@gmail.com>
2023-06-24 00:03:46 +0900

    fix: script, appspec수정

--- scripts/deploy-before.sh
+++ scripts/deploy-before.sh
@@ -4,3 +4,8 @@ if [ -d /var/www/insightout ]; then
     sudo rm -rf /var/www/insightout
 fi
 sudo mkdir -vp /var/www/insightout
+
+# yarn install -> node_modules는 복사 안함
+yarn
+# prisma generate
+yarn prisma generate
