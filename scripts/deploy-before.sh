197ffb1ad0d4c98eaf6f50c5dc4ac9586c285d02
PracticeEveryday <developerforus@gmail.com>
2023-06-24 01:32:07 +0900

    fix: deploy-before.sh 수정

--- scripts/deploy-before.sh
+++ scripts/deploy-before.sh
@@ -9,6 +9,9 @@
 echo "🙏 /home/ec2-user/InsightOut-Server"
 cd /home/ec2-user/InsightOut-Server
 
+echo "---- ls -al"
+ls -al
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
