e745f5c27270890b9bdd8f352928ca130ab490b1
PracticeEveryday <developerforus@gmail.com>
2023-06-24 00:47:25 +0900

    fix: deploy-before.sh

--- scripts/deploy-before.sh
+++ scripts/deploy-before.sh
@@ -5,6 +5,10 @@
 #fi
 #sudo mkdir -vp /var/www/insightout
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 
 # ec2-user 권한 부여
 #echo "권한 부여"
@@ -20,8 +24,8 @@
 #npm install -g yarn
 
 # yarn install -> node_modules는 복사 안함
-#echo "yarn"
-#yarn
+echo "yarn"
+yarn
 
 # prisma generate
 yarn prisma generate
