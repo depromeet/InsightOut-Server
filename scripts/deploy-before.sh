af616df70502e94e6cc45170dcaa0862ea87a538
PracticeEveryday <developerforus@gmail.com>
2023-06-24 00:08:22 +0900

    fix deploy-before.sh 권한 부여

--- scripts/deploy-before.sh
+++ scripts/deploy-before.sh
@@ -5,6 +5,9 @@ if [ -d /var/www/insightout ]; then
 fi
 sudo mkdir -vp /var/www/insightout
 
+
+//ec2-user 권한 부여
+sudo chown ec2-user:ec2-user /var/www/
 # yarn install -> node_modules는 복사 안함
 yarn
 # prisma generate
