9ab0fe196a15987b53fa567fefc61d5238be2a9f
kimddakki <97580759+PracticeEveryday@users.noreply.github.com>
2023-06-24 18:11:30 +0900

    Feature/#218/cicd (#219)
    - #218 main으로 PUSH시 자동 배포 스크립트

--- deploy-before.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo ------------------------------------
-if [ -d /home/ubuntu/13th-4team-backend ]; then
-    sudo rm -rf /home/ubuntu/13th-4team-backend
-fi
-sudo mkdir -vp /home/ubuntu/13th-4team-backend
