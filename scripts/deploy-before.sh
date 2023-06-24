79b669de694ac663994e0c3cd9581391ba397cbf
PracticeEveryday <developerforus@gmail.com>
2023-06-24 16:17:23 +0900

    fix: deploy-befor.sh 수정

--- scripts/deploy-before.sh
+++ scripts/deploy-before.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-#echo ------------------------------------
-#echo ------------------------------------
-#if [ -d /home/ec2-user/InsightOut-Server ]; then
-#    sudo rm -rf /home/ec2-user/InsightOut-Server
-#fi
-#sudo mkdir -vp /home/ec2-user/InsightOut-Server
 
 # cd /var/www/insightout
 echo "🙏 /home/ec2-user/InsightOut-Server"
@@ -14,12 +8,6 @@ cd /home/ec2-user/InsightOut-Server
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-# ec2-user 권한 부여
-#echo "‍👨‍💻 권한 부여"
-#sudo chown ec2-user:ec2-user /home/ec2-user/InsightOut-Server
-#sudo chmod -R 777 /home/ec2-user/InsightOut-Server
-
 
 # yarn install -> node_modules는 복사 안함
 echo "🤏 yarn"
