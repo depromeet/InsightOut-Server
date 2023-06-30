eda0eb540dd9e7523f90a3df021daa68651a1814
Seongtae <stae1102@gmail.com>
2023-06-30 16:02:52 +0900

    [Feature] 직무 선택 온보딩 경험 여부 컬럼 추가 & 회원가입/로그인 시 반환되는 온보딩 값에 직무 선택 온보딩 경험 여부 추가 (#231)
    * feat: onboarding 테이블에 field 컬럼 추가
    
    * feat: signin response dto에 field 추가
    
    * feat: 온보딩 수정 시 직무 선택 온보딩 경험 여부도 반환

--- src/apps/server/onboarding/dtos/get-onboarding.dto.ts
+++ src/apps/server/onboarding/dtos/get-onboarding.dto.ts
@@ -4,12 +4,14 @@ import { Exclude, Expose } from 'class-transformer';
 import { IsBoolean, IsNotEmpty } from 'class-validator';
 
 export class GetAllOnboardingsResponseDto {
+  @Exclude() private readonly _field: boolean;
   @Exclude() private readonly _experience: boolean;
   @Exclude() private readonly _experienceStepper: boolean;
   @Exclude() private readonly _resume: boolean;
   @Exclude() private readonly _collection: boolean;
 
   constructor(onboarding: Onboarding) {
+    this._field = onboarding.field;
     this._experience = onboarding.experience;
     this._experienceStepper = onboarding.experienceStepper;
     this._resume = onboarding.resume;
@@ -18,7 +20,21 @@ export class GetAllOnboardingsResponseDto {
 
   @Expose()
   @ApiProperty({
+    description: '직무 선택 온보딩 여부',
+    example: false,
+    type: Boolean,
+  })
+  @IsBoolean()
+  @IsNotEmpty()
+  get field(): boolean {
+    return this._field;
+  }
+
+  @Expose()
+  @ApiProperty({
     description: '경험 분해 온보딩 여부',
+    example: false,
+    type: Boolean,
   })
   @IsBoolean()
   @IsNotEmpty()
@@ -29,6 +45,8 @@ export class GetAllOnboardingsResponseDto {
   @Expose()
   @ApiProperty({
     description: '경험 분해 스텝퍼 온보딩 여부',
+    example: false,
+    type: Boolean,
   })
   @IsBoolean()
   @IsNotEmpty()
@@ -39,6 +57,8 @@ export class GetAllOnboardingsResponseDto {
   @Expose()
   @ApiProperty({
     description: '자기소개서 온보딩 여부',
+    example: false,
+    type: Boolean,
   })
   @IsBoolean()
   @IsNotEmpty()
@@ -49,6 +69,8 @@ export class GetAllOnboardingsResponseDto {
   @Expose()
   @ApiProperty({
     description: '모아보기 온보딩 여부',
+    example: false,
+    type: Boolean,
   })
   @IsBoolean()
   @IsNotEmpty()
