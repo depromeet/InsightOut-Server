df6d2092afa510bd56b50c9f3bce172be35fc579
Seongtae <stae1102@gmail.com>
2023-07-31 22:16:30 +0900

    feat: vercel sdk를 사용하여 ai 응답 구현

--- src/libs/modules/open-ai/open-ai.service.ts
+++ src/libs/modules/open-ai/open-ai.service.ts
@@ -6,12 +6,17 @@ import { catchError, firstValueFrom } from 'rxjs';
 import { openAIModelEnum } from '📚libs/modules/open-ai/openAIModel.enum';
 import { AiResponse } from '📚libs/modules/open-ai/interface/aiResponse.interface';
 import { MINUTES } from '🔥apps/server/common/consts/time.const';
+import { Configuration, OpenAIApi } from 'openai';
+import { OpenAIStream, StreamingTextResponse, streamToResponse } from 'ai';
+import { Response } from 'express';
 
 @Injectable()
 export class OpenAiService {
   private openAIHeader: { [key in string] };
   private OPEN_AI_URL: string;
   private OPEN_AI_MODEL = openAIModelEnum.GPT_3DOT5_TERBO;
+  private configuration: Configuration;
+  private openai;
 
   constructor(private readonly envService: EnvService, private readonly httpService: HttpService) {
     this.openAIHeader = {
@@ -19,6 +24,10 @@ export class OpenAiService {
       Authorization: `Bearer ${this.envService.get(EnvEnum.OPENAI_API_KEY)}`,
     };
     this.OPEN_AI_URL = this.envService.get(EnvEnum.OPENAI_CHAT_URL);
+    this.configuration = new Configuration({
+      apiKey: envService.get(EnvEnum.OPENAI_API_KEY),
+    });
+    this.openai = new OpenAIApi(this.configuration);
   }
 
   public async promptChatGPT(content: string): Promise<AiResponse> {
@@ -44,5 +53,17 @@ export class OpenAiService {
     const responseData = response?.data;
 
     return responseData as AiResponse;
+  }
+
+  public async getStreamGPTResponse(content: string) {
+    const aiResponse = await this.openai.createChatCompletion({
+      model: this.OPEN_AI_MODEL,
+      messages: [{ role: 'user', content }],
+      stream: true,
+    });
+
+    const stream = OpenAIStream(aiResponse);
+
+    return new StreamingTextResponse(stream);
   }
 }
