6113f62d20b01d48bba878536bffc30400cb7e2a
Seongtae <stae1102@gmail.com>
2023-09-23 18:26:10 +0900

    ci: codedeploy 스크립트 수정

--- scripts/deploy-before.sh
+++ scripts/deploy-before.sh
@@ -9,5 +9,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-yarn prisma generate
-yarn prisma db push
+node node_modules/prisma/build/index.js generate
+node node_modules/prisma/build/index.js db push
