f5928cbc9a6acbf443e1dc7a806b0b5c11f58817
Seongtae <stae1102@gmail.com>
2023-09-23 16:58:31 +0900

    ci: deploy before 스크립트 수정

--- scripts/deploy-before.sh
+++ scripts/deploy-before.sh
@@ -9,10 +9,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-# yarn install -> node_modules는 복사 안함
-echo "🤏 yarn"
-yarn
-
-# prisma generate
 yarn prisma generate
 yarn prisma db push
