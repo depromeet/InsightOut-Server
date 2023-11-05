5436dccdaa7b0eec5c958402552b552df4b5abae
Seongtae <stae1102@gmail.com>
2023-11-05 17:02:02 +0900

    feat: capability 엔티티 타입을 prisma에서 공용 entity로 변경

--- src/libs/modules/database/repositories/capability/capability.interface.ts
+++ src/libs/modules/database/repositories/capability/capability.interface.ts
@@ -1,11 +1,11 @@
-import { Capability, Prisma } from '@prisma/client';
+import { Capability } from '@libs/modules/database/entities/capability/capability.entity';
+import { KeywordType } from '@libs/modules/database/entities/capability/capabilityEntity.interface';
 
 export interface CapabilityRepository {
-  findMany(condition: { where: Partial<Capability> });
-  findFirst(condition: { where: Partial<Capability> });
-  create(data: Prisma.CapabilityCreateArgs);
+  save(capability: Capability): Promise<Partial<Capability>>;
+  findOneByUserIdAndKeyword(userId: number, keyword: string, keywordType?: KeywordType): Promise<Partial<Capability>>;
   countExperienceAndCapability(userId: number, isCompleted?: boolean);
-  findByUserId(userId: number): Promise<Capability[]>;
+  findByUserId(userId: number): Promise<Partial<Capability>[]>;
 }
 
 export const CapabilityRepository = Symbol('CapabilityRepository');
